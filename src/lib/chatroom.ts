586c4f98f60b6e22466474c462f5ac76ae55d8aa
boosterz <gnlcks7949@gmail.com>
2024-04-13 16:26:07 +0900

    Simplify chatroom methods and data structures
    Reduced and simplified numerous methods in Chatroom.ts, removing redundant or bloated code to increase understanding and efficiency. The file's structure has been revised to streamline the way it interacts with firestore. The firebase configuration has also been moved to a more appropriate location within the `src/lib/` directory, enhancing modularity.

--- src/lib/chatroom.ts
+++ src/lib/chatroom.ts
@@ -2,7 +2,6 @@ import firestore from "./firebase-config";
 import {collection, getDocs} from "firebase/firestore/lite";
 
 export type Message = {
-
     id: string,
     user : string,
     content: string,
@@ -11,51 +10,13 @@ export type Message = {
 }
 
 export type Chatroom = {
-  id: string;
-  name: string;
-  messages: Message[];
-};
+    id: string,
+    name: string,
+    messages: Message[],
+}
 
 export async function getChatrooms() {
-  const chatrooms = await firestore.collection("chatrooms").get();
 
-<<<<<<< HEAD
-  const chatroomList: Chatroom[] = [];
-
-  chatrooms.docs.forEach((chatroom: any) => {
-    chatroomList.push({
-      id: chatroom.id,
-      name: chatroom.data().name,
-      messages: chatroom.data().messages,
-    });
-  });
-
-  return chatroomList;
-}
-
-export async function createChatroom(name: string) {
-  const uuid = Math.random().toString(36).substring(7);
-  const chatroom = await firestore.collection("chatrooms").doc(uuid).set({
-    name: name,
-    messages: [],
-  });
-}
-
-export async function createChatroomMessage(
-  chatroomId: string,
-  message: Message
-) {
-  const chatroom = await firestore
-    .collection("chatrooms")
-    .doc(chatroomId)
-    .get();
-  const messages = chatroom.data().messages;
-  messages.push(message);
-  await firestore.collection("chatrooms").doc(chatroomId).update({
-    messages: messages,
-  });
-}
-=======
     const chatrooms = collection(firestore, "chatrooms")
     const chatroomSnapshot = await getDocs(chatrooms);
     const chatroomList  = chatroomSnapshot.docs.map((doc) => {
@@ -63,7 +24,7 @@ export async function createChatroomMessage(
     } );
 
 
-return chatroomList.map(chatroomFromDoc);
+    return chatroomList.map(chatroomFromDoc);
 
 
 }
@@ -94,4 +55,3 @@ function messageFromDoc(data: any) {
         createdAt: data.createdAt ?? new Date(),
     } as Message;
 }
->>>>>>> bffca8ee0e2f209003bed878911e3f6540162b1b
