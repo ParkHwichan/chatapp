ae3a475ac8122f0f836c9da1f3e7133f0c479a04
유성원 <swyoo753@naver.com>
2024-04-13 16:22:15 +0900

    for merge

--- src/lib/chatroom.ts
+++ src/lib/chatroom.ts
@@ -1,59 +1,53 @@
 import firestore from "./firebase-config";
 
 export type Message = {
-    id: string,
-    user : string,
-    content: string,
-    createdAt: string,
-
-}
+  id: string;
+  user: string;
+  content: string;
+  createdAt: string;
+};
 
 export type Chatroom = {
-    id: string,
-    name: string,
-    messages: Message[],
-}
+  id: string;
+  name: string;
+  messages: Message[];
+};
 
 export async function getChatrooms() {
+  const chatrooms = await firestore.collection("chatrooms").get();
 
-    const chatrooms = await firestore.collection("chatrooms").get();
+  const chatroomList: Chatroom[] = [];
 
-
-    const chatroomList: Chatroom[] = [];
-
-    chatrooms.docs.forEach((chatroom : any) => {
-        chatroomList.push({
-            id: chatroom.id,
-            name: chatroom.data().name,
-            messages: chatroom.data().messages,
-        });
+  chatrooms.docs.forEach((chatroom: any) => {
+    chatroomList.push({
+      id: chatroom.id,
+      name: chatroom.data().name,
+      messages: chatroom.data().messages,
     });
+  });
 
-    return chatroomList;
-
+  return chatroomList;
 }
 
-
-export async function createChatroom(
-    name: string,
-) {
-
-    const uuid = Math.random().toString(36).substring(7);
-    const chatroom = await firestore.collection("chatrooms").doc(uuid).set({
-        name : name,
-        messages: [],
-    });
+export async function createChatroom(name: string) {
+  const uuid = Math.random().toString(36).substring(7);
+  const chatroom = await firestore.collection("chatrooms").doc(uuid).set({
+    name: name,
+    messages: [],
+  });
 }
 
 export async function createChatroomMessage(
-    chatroomId: string,
-    message: Message
+  chatroomId: string,
+  message: Message
 ) {
-    const chatroom = await firestore.collection("chatrooms").doc(chatroomId).get();
-    const messages = chatroom.data().messages;
-    messages.push(message);
-    await firestore.collection("chatrooms").doc(chatroomId).update({
-        messages: messages,
-    });
+  const chatroom = await firestore
+    .collection("chatrooms")
+    .doc(chatroomId)
+    .get();
+  const messages = chatroom.data().messages;
+  messages.push(message);
+  await firestore.collection("chatrooms").doc(chatroomId).update({
+    messages: messages,
+  });
 }
-
