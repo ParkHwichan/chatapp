bffca8ee0e2f209003bed878911e3f6540162b1b
boosterz <gnlcks7949@gmail.com>
2024-04-13 16:18:56 +0900

    .ts`) have been refactored for clarity and efficiency. Additionally, the firebase configuration file has been relocated from the global scope to the `src/lib/` directory.

--- README.md
+++ README.md
@@ -1,36 +1,21 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+### 구조 
+redux state 
 
-## Getting Started
+chatrooms {
+    messages : {
+        createdAt : Date,
+    }[]
+}
 
-First, run the development server:
+### 폴링
+noSQL이니까 일정주기(ex 5초) firestore 쿼리를 날림
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+모든 챗룸에 대해서 가장 최근 메세지의 createdAt 보다 큰 메세지들 
+있으면 어레이에 추가하고,
+로컬에서 notReadedCount를 증가시킴
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+(선택)만약 새로고침해도 notReadedCount가 유지되려면 redux-persist를 사용해야함
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+### CRUD 구현
+채팅룸, 채팅에 대한 CRUD구현 +
+redux상태 관리
--- src/lib/chatroom.ts
+++ src/lib/chatroom.ts
@@ -1,10 +1,11 @@
 import firestore from "./firebase-config";
+import {collection, getDocs} from "firebase/firestore/lite";
 
 export type Message = {
     id: string,
     user : string,
     content: string,
-    createdAt: string,
+    createdAt: Date,
 
 }
 
@@ -16,44 +17,41 @@ export type Chatroom = {
 
 export async function getChatrooms() {
 
-    const chatrooms = await firestore.collection("chatrooms").get();
+    const chatrooms = collection(firestore, "chatrooms")
+    const chatroomSnapshot = await getDocs(chatrooms);
+    const chatroomList  = chatroomSnapshot.docs.map((doc) => {
+        return doc.data();
+    } );
 
 
-    const chatroomList: Chatroom[] = [];
+return chatroomList.map(chatroomFromDoc);
 
-    chatrooms.docs.forEach((chatroom : any) => {
-        chatroomList.push({
-            id: chatroom.id,
-            name: chatroom.data().name,
-            messages: chatroom.data().messages,
-        });
-    });
-
-    return chatroomList;
 
 }
 
+function chatroomFromDoc(data: any) {
 
-export async function createChatroom(
-    name: string,
-) {
+    try {
+        return {
+            id : data.id ?? "-1",
+            name: data.name ?? "Unknown",
+            messages: data.messages ? data.messages.map(messageFromDoc) : [] as Message[],
+        } as Chatroom;
+    }  catch (e) {
+        return {
+            id: "-1",
+            name: "Unknown",
+            messages: [] as Message[],
+        } as Chatroom;
+    }
 
-    const uuid = Math.random().toString(36).substring(7);
-    const chatroom = await firestore.collection("chatrooms").doc(uuid).set({
-        name : name,
-        messages: [],
-    });
 }
 
-export async function createChatroomMessage(
-    chatroomId: string,
-    message: Message
-) {
-    const chatroom = await firestore.collection("chatrooms").doc(chatroomId).get();
-    const messages = chatroom.data().messages;
-    messages.push(message);
-    await firestore.collection("chatrooms").doc(chatroomId).update({
-        messages: messages,
-    });
+function messageFromDoc(data: any) {
+    return {
+        id: data.id ?? "-1",
+        user: data.user ?? "Unknown",
+        content: data.content ?? "Unknown",
+        createdAt: data.createdAt ?? new Date(),
+    } as Message;
 }
-
